49c8a6eaf715ece0577d4dc5a969bf3765f01ba5
dealhouse <de.juan1@live.com>
2022-06-26 11:42:55 -0400

    added dependencies

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,8 +1,10 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 
 function Popular() {
+
+    const [popular, setPopular] = useState([])
 
     useEffect(() => {
         getPopular()
@@ -12,10 +14,17 @@ function Popular() {
         const api = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
         const data = await api.data
         console.log(data)
+        setPopular(data.recipes)
     }
 
   return (
-    <div>Popular</div>
+    <div>
+        {popular.map((recipe) => (
+            <div key={recipe.id} className='card'>
+                <p>{recipe.title}</p>
+            </div>
+        ))}
+    </div>
   )
 }
 
